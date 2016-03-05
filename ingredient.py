c6af36679984c8f279edaf9fe18246343cdba2e8
Neal Khosla <nealkhosla@DN525fh1.SUNet>
2016-03-04 16:26:01 -0800

    made subclasses call superclass

--- ingredient.py
+++ ingredient.py
@@ -1,39 +1,41 @@
 class Ingredient:
-	name = ""
-	keywords = []
+	def __init__(self, name="Ingredient", keywords=[]):
+		self.name = name
+		self.keywords = keyword
+
 
 class Grain(Ingredient):
-	name = "Grain"
-	keywords = []
+	def __init__(self, name="Grain", keywords=[]):
+		super(Grain, self).__init__(name, keywords)
 
 class Lacto(Ingredient):
-	name = "Lacto"
-	keywords = []
+	def __init__(self, name="Lacto", keywords=[]):
+		super(Lacto, self).__init__(name, keywords)
 
 class Ovo(Ingredient):
-	name = "Ovo"
-	keywords = []
+	def __init__(self, name="Ovo", keywords=[]):
+		super(Ovo, self).__init__(name, keywords)
 
 class Meat(Ingredient):
-	name = "Meat"
-	keywords = []
+	def __init__(self, name="Meat", keywords=[]):
+		super(Meat, self).__init__(name, keywords)
 
 class Vegetable(Ingredient):
-	name = "Vegetable"
-	keywords = []
+	def __init__(self, name="Vegetable", keywords=[]):
+		super(Vegetable, self).__init__(name, keywords)
 
 class Fruit(Ingredient):
-	name = "Fruit"
-	keywords = []
+	def __init__(self, name="Fruit", keywords=[]):
+		super(Fruit, self).__init__(name, keywords)
 
 class Poultry(Meat):
-	name = "Poultry"
-	keywords = []
+	def __init__(self, name="Poultry", keywords=[]):
+		super(Poultry, self).__init__(name, keywords)
 
 class RedMeat(Meat):
-	name = "RedMeat"
-	keywords = []
+	def __init__(self, name="RedMeat", keywords=[]):
+		super(RedMeat, self).__init__(name, keywords)
 
 class Seafood(Meat):
-	name = "Seafood"
-	keywords = []
+	def __init__(self, name="Seafood", keywords=[]):
+		super(Seafood, self).__init__(name, keywords)
