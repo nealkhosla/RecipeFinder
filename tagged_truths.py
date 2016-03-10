4ebf8f2c14ae178aaf716339f1801fc11cdebc60
chrismin <chrismin@stanford.edu>
2016-03-09 19:17:18 -0800

    tagged first 50 recipes

---
+++ tagged_truths.py
@@ -0,0 +1,411 @@
+# python dict of recipeID -> list of appropriate diets
+import diet
+
+
+truths = {
+	"5160756b96cc62079cc2db15" : [
+		diet.NonVegetarianDiet
+	], 
+
+	"5160756d96cc62079cc2db16" : [
+		diet.NonVegetarianDiet
+	],
+
+	"5160756f96cc6207a37ff777" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.VeganDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.LactoseIntolDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160757096cc62079cc2db17" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+	],
+
+	"5160757496cc6207a37ff778" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+	],
+
+	"5160757596cc62079cc2db18" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+	],
+
+	"5160757596cc6207aada322f" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160757696cc62079cc2db19" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoOvoDiet,
+		diet.LactoseIntolDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160757696cc6207a37ff779" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.VeganDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.LactoseIntolDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160757796cc6207aada3230" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+	],
+
+	"5160757896cc62079cc2db1a" : [
+		diet.NonVegetarianDiet,
+		diet.LactoseIntolDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160757a96cc62079cc2db1b": [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.VeganDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.LactoseIntolDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160757a96cc6207b12d18da" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+	],
+
+	"5160757b96cc6207a37ff77a" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+	],
+
+	"5160757b96cc6207aada3231" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+	],
+
+	"5160757c96cc62079cc2db1c" : [
+		diet.NonVegetarianDiet,
+	],
+
+	"5160757d96cc6207a37ff77b" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160757d96cc6207aada3232" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.VeganDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.LactoseIntolDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160757d96cc6207b12d18db" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+	],
+
+	"5160757f96cc62079cc2db1d" : [
+		diet.NonVegetarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160757f96cc6207b12d18dc" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+	],
+
+	"5160758096cc6207a37ff77c" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.VeganDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.LactoseIntolDiet,
+		diet.PescatarianDiet,
+	],
+
+	"5160758296cc6207aada3233" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.VeganDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.LactoseIntolDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160758296cc6207b12d18dd" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160758396cc6207a37ff77d" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160758396cc6207b8ac8431" : [
+		diet.NonVegetarianDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160758496cc6207a37ff77e" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160758496cc6207aada3234" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160758596cc6207b12d18de" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160758696cc6207b8ac8432" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+	],
+
+	"5160758796cc6207a37ff77f" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet,
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+	],
+
+	"5160758896cc6207aada3235" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160758896cc6207b12d18df" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160758996cc6207b8ac8433" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160758a96cc6207aada3236" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160758b96cc6207b12d18e0" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+	],
+
+	"5160758b96cc6207b8ac8434" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoOvoDiet,
+		diet.LactoseIntolDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160758c96cc6207b12d18e1" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160758d96cc6207a37ff780" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.VeganDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.LactoseIntolDiet,
+		diet.PescatarianDiet,
+	],
+
+	"5160758f96cc6207b12d18e2" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.VeganDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.LactoseIntolDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160759096cc6207b8ac8435" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.VeganDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.LactoseIntolDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"5160759296cc6207a37ff781" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"516075da96cc6208b330012c" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+	],
+
+	"516075dd96cc6208b330012d" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.VeganDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.LactoseIntolDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"516075df96cc6208b330012e" : [
+		diet.NonVegetarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"516075e096cc6208ba280483" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+	],
+
+	"516075e296cc6208b330012f" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+	],
+
+
+	"516075e496cc6208b3300130" : [
+		diet.NonVegetarianDiet,
+		diet.VegetarianDiet, 
+		diet.VeganDiet, 
+		diet.LactoseIntolDiet,
+		diet.LactoDiet,
+		diet.LactoOvoDiet,
+		diet.PescatarianDiet,
+		diet.GlutenFreeDiet,
+	],
+
+	"516075e696cc6208b3300131" : [
+		diet.NonVegetarianDiet,
+	],
+
+	"516075e896cc6208b3300132" : [
+		diet.NonVegetarianDiet,
+	],
+
+}
